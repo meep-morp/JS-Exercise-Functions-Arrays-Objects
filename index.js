59753ef0d1d956ac7c506817014a6c555a7c2b2b
meep-morp <gracieaerogirl@gmail.com>
2020-04-16 16:01:21 -0500

    Finished task 5

--- index.js
+++ index.js
@@ -169,12 +169,13 @@ function isItAnApple(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === "apple") {
       bool.push(true);
-    } else bool.push(false);
+    } else { bool.push(false); 
+      }
   }
   return bool;
   }
 
-console.log(isItAnApple([ 'orange', 'apple', 'banana', 'apples', 'apple', 'mango' ]));
+console.log(isItAnApple([ 'orange', 'apple', 'apple', 'apples', 'apple', 'mango' ]));
 
 
 /*
@@ -230,9 +231,28 @@ function get3rdCar(inventory) {
  * it will return `This is a Lincoln Navigator`.
 */
 function getCarInfoByIndex(inventory, index) {
-  /* code here */
+  for (let i = 0; i < inventory.length; i++) {
+    if (index === i) {
+      return `This is a ${inventory[i].car_make} ${inventory[i].car_model}`;
+    }
+  }
 }
-
+console.log(getCarInfoByIndex([
+  { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+  { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+  { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+  { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+  { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+  { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+  { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+  { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+  { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+  { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+  { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+  { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
+  { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
+  { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
+], 4))
 /**
  * ### Challenge `getLastCarInfo`
  * 
