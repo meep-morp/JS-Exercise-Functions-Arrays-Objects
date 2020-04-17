97f2b5b9791bdb4180240093df3d43ce52cb7356
meep-morp <gracieaerogirl@gmail.com>
2020-04-16 20:30:44 -0500

    Finished Stretch #3

--- index.js
+++ index.js
@@ -242,22 +242,7 @@ function getCarInfoByIndex(inventory, index) {
     }
   }
 }
-// console.log(getCarInfoByIndex([
-//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-//   { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
-//   { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-//   { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
-//   { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-//   { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
-//   { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
-//   { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
-//   { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
-//   { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
-//   { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
-//   { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
-//   { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
-// ], 5))
+
 /**
  * ### Challenge `getLastCarInfo`
  * 
@@ -276,22 +261,7 @@ function getLastCarInfo(arr) {
     }
   }
 }
-// console.log(getLastCarInfo([
-//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-//   { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
-//   { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-//   { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
-//   { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-//   { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
-//   { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
-//   { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
-//   { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
-//   { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
-//   { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
-//   { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
-//   { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
-// ]));
+
 /**
  * ### Challenge `getModelYears`
  * 
@@ -308,22 +278,6 @@ function getModelYears(arr) {
   }
   return years;
 }
-// console.log(getModelYears([
-//   { id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
-//   { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
-//   { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
-//   { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
-//   { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
-//   { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
-//   { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
-//   { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
-//   { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
-//   { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
-//   { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
-//   { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 },
-//   { id: 13, car_make: "Chevrolet", car_model: "Cavalier", car_year: 1997 },
-//   { id: 14, car_make: "Dodge", car_model: "Ram Van 1500", car_year: 1999 }
-// ]));
 
 /**
  * ### Challenge `getCarInfoById`
@@ -385,9 +339,24 @@ function getOlderCars(arr, year) {
  * made by either `Audi` or `Mercedes-Benz` or `Volkswagen` or `BMW`,
  * in the same order as they appear in the original inventory.
 */
-function getGermanCars(/* code here */) {
-  /* code here */
+function getGermanCars(arr) {
+  const germany = [];
+  for (let i = 0; i < arr.length; i++){
+    
+  }
 }
+// console.log(getGermanCars([{ id: 1, car_make: "Lincoln", car_model: "Navigator", car_year: 2009 },
+// { id: 2, car_make: "Mazda", car_model: "Miata MX-5", car_year: 2001 },
+// { id: 3, car_make: "Land Rover", car_model: "Defender Ice Edition", car_year: 2010 },
+// { id: 4, car_make: "Honda", car_model: "Accord", car_year: 1983 },
+// { id: 5, car_make: "Mitsubishi", car_model: "Galant", car_year: 1990 },
+// { id: 6, car_make: "Honda", car_model: "Accord", car_year: 1995 },
+// { id: 7, car_make: "Smart", car_model: "Fortwo", car_year: 2009 },
+// { id: 8, car_make: "Audi", car_model: "4000CS Quattro", car_year: 1987 },
+// { id: 9, car_make: "Ford", car_model: "Windstar", car_year: 1996 },
+// { id: 10, car_make: "Mercedes-Benz", car_model: "E-Class", car_year: 2000 },
+// { id: 11, car_make: "Infiniti", car_model: "G35", car_year: 2004 },
+// { id: 12, car_make: "Lotus", car_model: "Esprit", car_year: 2004 }]));
 
 /**
  * ### Challenge `carMaker`
@@ -402,8 +371,14 @@ function getGermanCars(/* code here */) {
  *         (1) causes the odometer in the object to be increased by the distance,
  *         (2) returns the updated value of the `odometer`.
 */
-function carMaker(/* code here */) {
-  /* code here */
+function carMaker(distance) {
+  return {
+    odometer: distance,
+    drive: function(distance) {
+      this.odometer = this.odometer + distance;
+      return this.odometer;
+    }
+  }
 }
 
 /// ////// END OF CHALLENGE /////////
